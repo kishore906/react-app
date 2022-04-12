ae42ccfe28bbae329162be7318c38df57d0d4ceb
kishore <kkishore906@gmail.com>
2022-04-12 23:42:30 +1000

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img
+          src="https://www.propelleraero.com/wp-content/uploads/2021/05/Vector.svg"
+          className="App-logo"
+          alt="logo"
+        />
+        <p>Welcome to propelleraero</p>
+        <a
+          className="App-link"
+          href=" https://www.propelleraero.com/dirtmate/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Visit Page
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
